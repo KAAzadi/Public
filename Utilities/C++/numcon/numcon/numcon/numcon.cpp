481f66b8c465571269270a7d3b3b020e34af3e4e
KAAzadi <70913312+KAAzadi@users.noreply.github.com>
2023-02-26 00:28:04 -0500

    First Pass done
    Able to convert some test values by hand (may consider unit testing in the future).

--- Utilities/C++/numcon/numcon/numcon/numcon.cpp
+++ Utilities/C++/numcon/numcon/numcon/numcon.cpp
@@ -8,13 +8,16 @@
 #define FAILURE -1
 
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 //prototypes
 int Chart(char value);
+char Chart2(int value);
 bool CheckInput(int argc, char* args[]);
-int ConvertValue(char* value, int factor);
+void ConvertValue(char flag1, char flag2, char* value);
+int GetFactor(char flag);
 void PrintMenu();
 
 //start
@@ -87,27 +90,125 @@ int Chart(char value)
     }
 }
 
+char Chart2(int value)
+{
+    switch (value) {
+    case  1:
+        return '1';
+    case 2:
+        return '2';
+    case 3:
+        return '3';
+    case 4:
+        return '4';
+    case 5:
+        return '5';
+    case 6:
+        return '6';
+    case 7:
+        return '7';
+    case 8:
+        return '8';
+    case 9:
+        return '9';
+    case 10:
+        return 'A';
+    case 11:
+        return 'B';
+    case 12:
+        return 'C';
+    case 13:
+        return 'D';
+    case 14:
+        return 'E';
+    case 15:
+        return 'F';
+    default:
+        cout << "Somehow we got here.\n";
+        return 'Z';
+    }
+}
+
 bool CheckInput(int argc, char* args[]) {
     if (args[1][1] == 'h') {
         PrintMenu();
+        return false;
     }
     else if (argc < 5) {
-        cout << "Error: Not enough information\n Please enter \"numcon -h\" for a list of valid flags ";
+        cout << "Error: Not enough information.\n Please enter \"numcon -h\" for a list of valid flags and examples";
         PrintMenu();
+        return false;
+    }
+    else if (argc > 5) {
+        cout << "Error: Too much information.\n Please enter \"numcon -h\" for a list of valid flags and examples";
+        PrintMenu();
+        return false;
+    }
+    else {
+        return true;
     }
 }
 
 //main logic to create intermediate value
-int ConvertValue(char flag1, char flag2, char* value)
+void ConvertValue(char flag1, char flag2, char* value)
 {
-    int factor;
+    int factor = GetFactor(flag1);
     int total = 0;
-    for (int i = 0; i < strlen(value); i++) {
-        int temp = Chart(value[i]);
-        if (temp == -1) return FAILURE;
-        total = (total * factor) + Chart(value[i]);
+
+    if (factor != -1) {
+        for (int i = 0; i < strlen(value); i++) {
+            int temp = Chart(value[i]);
+            if (temp == -1) {
+                return;
+            }
+            total = (total * factor) + Chart(value[i]);
+        }
     }
-    return total;
+
+    factor = GetFactor(flag2);
+    string output = "";
+    if (factor != -1) {
+        int size = 0; //storing size as separate int to avoid time complexity of .size() in stack
+        stack<char> stack;
+        for (int i = 0; i < strlen(value); i++) {
+            if (total % factor == 0) {
+                stack.push('0');
+                
+            }
+            else {
+                stack.push(Chart2(total % factor));
+            }
+            size++;
+            total /= factor;
+        }
+        for (int i = 0; i < size; i++) {
+            output.push_back(stack.top());
+            stack.pop();
+        }
+    }
+
+    
+    cout << output;
+}
+
+int GetFactor(char flag) {
+    switch (flag) {
+    case 'b':
+        return 2;
+    case 'd':
+        return 10;
+    case 'h':
+        return 16;
+    case 'o':
+        return 8;
+    default:
+        cout << "Error: Invalid flag, please enter \"numcon -h\" for a list of valid flags\n";
+        return -1;
+    }
+}
+
+void PrintMenu() {
+
 }
 
 
